e589814edcfc448001f76a476d1bd63677a5387c
lilyhoratio <lilyhzhou@gmail.com>
2019-06-17 14:11:34 -0700

    added header image

--- js/index.js
+++ js/index.js
@@ -40,3 +40,9 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// let codeSnippet = document.getElementById("cta-img");
+// codeSnippet.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let codeSnippet = document.querySelector("#cta-img");
+codeSnippet.setAttribute('src', siteContent["cta"]["img-src"]);
